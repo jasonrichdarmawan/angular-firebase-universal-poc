38d738319760a3035050dd721997ea088bf818c5
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-09-18 20:04:27 +0700

    chore: add route /experiences and /experience/:experience

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "experiences",
+    loadChildren: () => import("./features/experiences/experiences.module").then(m => m.ExperiencesModule)
+  },
+  {
+    path: "experience",
+    loadChildren: () => import("./features/experience/experience.module").then(m => m.ExperienceModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/features/experience/experience-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ExperienceComponent } from './experience/experience.component';
+
+const routes: Routes = [
+  {
+    path: ":experience",
+    // TODO: loadComponent in Angular 14
+    component: ExperienceComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ExperienceRoutingModule { }
